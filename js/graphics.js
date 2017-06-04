c0ab5ea39fee5646cd0ed62879c6d7fa9c0df130
HikaruAikawa <crivtox@gmail.com>
2017-06-04 10:18:09 +0200

    Scaled down icons and added easier rescaling

--- js/graphics.js
+++ js/graphics.js
@@ -1,42 +1,75 @@
+// Initialization
+
 var imagesDiv = document.getElementById("imagesDiv");
+ctx.imageSmoothingEnabled = false;
 
-// Load images
+// Loading images
 
-// Contains true if the icon is loaded
-var isIconLoaded = {
-	"detective" : false,
-	"widow" : false
-}
-// Returns true if all icons are loaded
-function areAllIconsLoaded() {
-	var ret = true;
-	for (key in isIconLoaded) {
-		if (!isIconLoaded[key]) ret = false;
+	// Contains true if the icon is loaded
+	var isIconLoaded = {
+		"detective" : false,
+		"widow" : false,
+		"butler" : false,
+		"guest" : false,
+		"nephew" : false
 	}
-	return ret;
-}
+	// Returns true if all icons are loaded
+	function areAllIconsLoaded() {
+		var ret = true;
+		for (key in isIconLoaded) {
+			if (!isIconLoaded[key]) ret = false;
+		}
+		return ret;
+	}
 
-// Detective icon
-var detectiveIcon = new Image(64,64);
-detectiveIcon.onload = function() {
-	isIconLoaded["detective"] = true;
-	if (areAllIconsLoaded()) onAllIconsLoaded();
-};
-detectiveIcon.src = "assets/detective.png";
-imagesDiv.appendChild(detectiveIcon);
+	// Detective icon
+	var detectiveIcon = new Image();
+	detectiveIcon.onload = function() {
+		isIconLoaded["detective"] = true;
+		if (areAllIconsLoaded()) onAllIconsLoaded();
+	};
+	detectiveIcon.src = "assets/detective.png";
+	imagesDiv.appendChild(detectiveIcon);
 
-// Widow icon
-var widowIcon = new Image(64,64);
-widowIcon.onload = function() {
-	isIconLoaded["widow"] = true;
-	if (areAllIconsLoaded()) onAllIconsLoaded();
-};
-widowIcon.src = "assets/widow.png";
-imagesDiv.appendChild(widowIcon);
+	// Widow icon
+	var widowIcon = new Image();
+	widowIcon.onload = function() {
+		isIconLoaded["widow"] = true;
+		if (areAllIconsLoaded()) onAllIconsLoaded();
+	};
+	widowIcon.src = "assets/widow.png";
+	imagesDiv.appendChild(widowIcon);
+	
+	// Butler icon
+	var butlerIcon = new Image();
+	butlerIcon.onload = function() {
+		isIconLoaded["butler"] = true;
+		if (areAllIconsLoaded()) onAllIconsLoaded();
+	};
+	butlerIcon.src = "assets/butler.png";
+	imagesDiv.appendChild(butlerIcon);
+	
+	// Guest icon
+	var guestIcon = new Image();
+	guestIcon.onload = function() {
+		isIconLoaded["guest"] = true;
+		if (areAllIconsLoaded()) onAllIconsLoaded();
+	};
+	guestIcon.src = "assets/guest.png";
+	imagesDiv.appendChild(guestIcon);
+	
+	// Nephew icon
+	var nephewIcon = new Image();
+	nephewIcon.onload = function() {
+		isIconLoaded["nephew"] = true;
+		if (areAllIconsLoaded()) onAllIconsLoaded();
+	};
+	nephewIcon.src = "assets/nephew.png";
+	imagesDiv.appendChild(nephewIcon);
 
 // Executed when all icons are loaded
 function onAllIconsLoaded() {
-	ctx.drawImage(detectiveIcon,0,0);
-	ctx.drawImage(widowIcon,0,64);
+	ctx.drawImage(detectiveIcon,0,0,iconWidth,iconHeight);
+	ctx.drawImage(widowIcon,0,iconHeight,iconWidth,iconHeight);
 }
 
--- js/init.js
+++ js/init.js
@@ -1,2 +1,16 @@
+// Global variables
+
+var canvasWidth = 1024;
+var canvasHeight = 768;
+var iconWidth = 64;
+var iconHeight = 64;
+
+// Global references
+
 var canvas = document.getElementById("mainCanvas");
 var ctx = canvas.getContext("2d");
+
+// Global initialization
+
+canvas.width = canvasWidth;
+canvas.height = canvasHeight;
