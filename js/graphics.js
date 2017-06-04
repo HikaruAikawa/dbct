285637d3ad1a1cf8fd6605cf2c0c70068e6b7744
HikaruAikawa <crivtox@gmail.com>
2017-06-04 11:26:20 +0200

    Adapted the graphics script to work with any kind of image (previously only character icons)

--- js/graphics.js
+++ js/graphics.js
@@ -1,75 +1,47 @@
 // Initialization
 
 var imagesDiv = document.getElementById("imagesDiv");
+var images = {};
+
 ctx.imageSmoothingEnabled = false;
 
 // Loading images
 
-	// Contains true if the icon is loaded
-	var isIconLoaded = {
-		"detective" : false,
-		"widow" : false,
-		"butler" : false,
-		"guest" : false,
-		"nephew" : false
-	}
-	// Returns true if all icons are loaded
-	function areAllIconsLoaded() {
+	// Will contain true if the image is loaded
+	var isImageLoaded = {};
+	
+	// Returns true if all images are loaded
+	function areAllImagesLoaded() {
 		var ret = true;
-		for (key in isIconLoaded) {
-			if (!isIconLoaded[key]) ret = false;
+		for (key in isImageLoaded) {
+			if (!isImageLoaded[key]) ret = false;
 		}
 		return ret;
 	}
-
-	// Detective icon
-	var detectiveIcon = new Image();
-	detectiveIcon.onload = function() {
-		isIconLoaded["detective"] = true;
-		if (areAllIconsLoaded()) onAllIconsLoaded();
-	};
-	detectiveIcon.src = "assets/detective.png";
-	imagesDiv.appendChild(detectiveIcon);
-
-	// Widow icon
-	var widowIcon = new Image();
-	widowIcon.onload = function() {
-		isIconLoaded["widow"] = true;
-		if (areAllIconsLoaded()) onAllIconsLoaded();
-	};
-	widowIcon.src = "assets/widow.png";
-	imagesDiv.appendChild(widowIcon);
 	
-	// Butler icon
-	var butlerIcon = new Image();
-	butlerIcon.onload = function() {
-		isIconLoaded["butler"] = true;
-		if (areAllIconsLoaded()) onAllIconsLoaded();
-	};
-	butlerIcon.src = "assets/butler.png";
-	imagesDiv.appendChild(butlerIcon);
+	// Adds an image by image name
+	function addImage(imageName) {
+		var newImage = new Image();
+		newImage.onload = function() {
+			isImageLoaded[imageName] = true;
+			if (areAllImagesLoaded()) onAllImagesLoaded();
+		}
+		newImage.src = "assets/"+imageName+".png";
+		imagesDiv.appendChild(newImage);
+		images[imageName] = newImage;
+		isImageLoaded[imageName] = false;
+	}
 	
-	// Guest icon
-	var guestIcon = new Image();
-	guestIcon.onload = function() {
-		isIconLoaded["guest"] = true;
-		if (areAllIconsLoaded()) onAllIconsLoaded();
-	};
-	guestIcon.src = "assets/guest.png";
-	imagesDiv.appendChild(guestIcon);
-	
-	// Nephew icon
-	var nephewIcon = new Image();
-	nephewIcon.onload = function() {
-		isIconLoaded["nephew"] = true;
-		if (areAllIconsLoaded()) onAllIconsLoaded();
-	};
-	nephewIcon.src = "assets/nephew.png";
-	imagesDiv.appendChild(nephewIcon);
+	// Adding all character icons
+	addImage("detective");
+	addImage("widow");
+	addImage("butler");
+	addImage("guest");
+	addImage("nephew");
 
 // Executed when all icons are loaded
-function onAllIconsLoaded() {
-	ctx.drawImage(detectiveIcon,0,0,iconWidth,iconHeight);
-	ctx.drawImage(widowIcon,0,iconHeight,iconWidth,iconHeight);
+function onAllImagesLoaded() {
+	ctx.drawImage(images["detective"],0,0,iconWidth,iconHeight);
+	ctx.drawImage(images["widow"],0,iconHeight,iconWidth,iconHeight);
 }
 
