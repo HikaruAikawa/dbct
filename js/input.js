354799845b173711c731ab07bf519744249b9a77
HikaruAikawa <crivtox@gmail.com>
2017-09-13 07:25:13 +0200

    Added auxiliary function to check if the mouse click is in a rectangle

--- js/input.js
+++ js/input.js
@@ -2,14 +2,22 @@
 
 document.addEventListener("click", clickHandler);
 
+// Processes all click inputs
 function clickHandler (e) {
 	var clickX = e.clientX - canvasRect.x;
 	var clickY = e.clientY - canvasRect.y;
+	// Functions to check if the click is inside a given rectangle
+	function clickInRect(rectX, rectY, rectW, rectH) {
+		return clickX > rectX && clickX < rectX + rectW && clickY > rectY && clickY < rectY + rectH;
+	}
+	function clickInRect(rect) {
+		return clickX > rect.x && clickX < rect.x + rect.w && clickY > rect.y && clickY < rect.y + rect.h;
+	}
 	//testX = clickX;
 	//testY = clickY;
 	if (gameState == "dialogueInput") {
 		// If the click is inside the dialogue button
-		if (clickX > dialogueButton.x && clickX < dialogueButton.x + dialogueButton.w && clickY > dialogueButton.y && clickY < dialogueButton.y + dialogueButton.h) {
+		if (clickInRect(dialogueButton)) {
 			nextMoment();
 		}
 	}
